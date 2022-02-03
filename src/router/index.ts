a977636e1b833461c422f160db24901a15181c34
zhouhuafei <1123486116@qq.com>
2022-02-03 22:14:49 +0800

    feat: 🎸 微应用 - 主应用的404和子应用的404都多少存在一些问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,7 +13,13 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+  },
+  {
+    path: '*',
+    name: '404',
+    meta: { title: '404', hidden: true },
+    component: () => import(/* webpackChunkName: "404" */ '@/views/404.vue')
   }
 ]
 
