39d1ec6ae8c15a2851e32b5bc919f3e904df83b4
zhouhuafei <1123486116@qq.com>
2022-02-05 12:49:16 +0800

    feat: 🎸 子应用接收主应用传输过来的数据

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ let instance: any = null
 let history: any = null
 
 function render (props: any = {}) {
-  const { container } = props
+  const { container, mainStore } = props
+  console.log('child2 mainStore：', mainStore)
   // history = createWebHistory(process.env.BASE_URL)
   history = createWebHistory((window as any).__POWERED_BY_QIANKUN__ ? '/child2' : '/')
   router = createRouter({
